b43ef886796c0f0a5fc11dc77afd4ada302df42b
Luís Marques <lm@pop-os.localdomain>
2021-05-16 19:31:01 +0100

    Adds blockchain integrity validation

--- main.js
+++ main.js
@@ -37,10 +37,33 @@ class Blockchain {
 		newBlock.hash = newBlock.calculateHash();
 		this.chain.push(newBlock);
 	}
+
+	isChainValid() {
+		for (let i = 1; i < this.chain.length; i++) {
+			const currentBlock = this.chain[i];
+			const previousBlock = this.chain[i - 1];
+
+			if (currentBlock.hash !== currentBlock.calculateHash()) {
+				return false;
+			}
+
+			if (currentBlock.previousHash !== previousBlock.hash) {
+				return false;
+			}
+		}
+
+		return true;
+	}
 }
 
 let luCoin = new Blockchain();
 luCoin.addBlock(new Block(1, "08/05/2021", { amount: 6 }));
 luCoin.addBlock(new Block(2, "09/05/2021", { amount: 16 }));
 
-console.log(JSON.stringify(luCoin, null, 4));
+console.log(`Is blockchain valid? ${luCoin.isChainValid()}`);
+
+luCoin.chain[1].data = { amount: 100 };
+luCoin.chain[1].hash = luCoin.chain[1].calculateHash();
+console.log(`Is blockchain valid? ${luCoin.isChainValid()}`);
+
+// console.log(JSON.stringify(luCoin, null, 4));
